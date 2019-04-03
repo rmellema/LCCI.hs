3e5613c087e834fac330a9798693cf4806398176
rmellema <rene.mellema@gmail.com>
2019-04-03 15:31:19 +0200

    Updated the Hex module to the new structure

--- src/Hex.hs
+++ src/Hex.hs
@@ -8,15 +8,16 @@ import Issue
 import Model
 import Relation
 import Syntax
+import PrettySyntax
 
 -- | The worlds in the Hex model
-w1, w2, w3 :: World
-w1 = World 1
-w2 = World 2
-w3 = World 3
-w4 = World 4
-w5 = World 5
-w6 = World 6
+w1, w2, w3 :: World (Int, Int, Int)
+w1 = World (0, 1, 2)
+w2 = World (0, 2, 1)
+w3 = World (1, 0, 2)
+w4 = World (1, 2, 0)
+w5 = World (2, 0, 1)
+w6 = World (2, 1, 0)
 
 -- | The propositions in the Hex model
 a0, a1, a2, b0, b1, b2, c0, c1, c2 :: Proposition
@@ -30,14 +31,20 @@ c0 = proposition "C0"
 c1 = proposition "C1"
 c2 = proposition "C2"
 
+card :: Proposition -> String
+card = tail . show
+
+player :: Proposition -> Char
+player = head . show
+
 -- | Each world as a formula
 a0b1c2, a0b2c1, a1b0c2,a1b2c0,a2b0c1,a2b1c0 :: Formula
-a0b1c2 = And (Prop a0) (And (Prop b1) (Prop c2))
-a0b2c1 = And (Prop a0) (And (Prop b2) (Prop c1))
-a1b0c2 = And (Prop a1) (And (Prop b0) (Prop c2))
-a1b2c0 = And (Prop a1) (And (Prop b2) (Prop c0))
-a2b0c1 = And (Prop a2) (And (Prop b0) (Prop c1))
-a2b1c0 = And (Prop a2) (And (Prop b1) (Prop c0))
+a0b1c2 = a0 & b1 & c2
+a0b2c1 = a0 & b2 & c1
+a1b0c2 = a1 & b0 & c2
+a1b2c0 = a1 & b2 & c0
+a2b0c1 = a2 & b0 & c1
+a2b1c0 = a2 & b1 & c0
 
 -- | A list with all worlds as formulas
 forms = [a0b1c2, a0b2c1, a1b0c2, a1b2c0, a2b0c1, a2b1c0]
@@ -49,21 +56,17 @@ b = atom "b"
 c = atom "c"
 
 -- | The set of worlds
-ws :: Set.Set World
+ws :: Set.Set (World (Int, Int, Int))
 ws = Set.fromList [w1, w2, w3, w4, w5, w6]
 
 -- | The valuation of the propositional atoms
-v :: Valuation
-v = Map.fromList
-    [ (w1, Set.fromList [a0, b1, c2])
-    , (w2, Set.fromList [a0, b2, c1])
-    , (w3, Set.fromList [a1, b0, c2])
-    , (w4, Set.fromList [a1, b2, c0])
-    , (w5, Set.fromList [a2, b0, c1])
-    , (w6, Set.fromList [a2, b1, c0])
-    ]
+v :: Valuation (Int, Int, Int)
+v p (World (ca, cb, cc)) = show (card' (player p)) == card p
+    where card' 'A' = ca
+          card' 'B' = cb
+          card' 'C' = cc
 
-r :: Map.Map Atomic Relation
+r :: Map.Map Atomic (Relation (Int, Int, Int))
 r = Map.fromList
     [ (a, Relation.fromList
         [ (state [w1], state [w1]), (state [w1], state [w2])
@@ -92,5 +95,5 @@ r = Map.fromList
     ]
 
 -- | The actual model
-m :: StaticModel
-m = StaticModel ws v r
+hex :: StaticModel (Int, Int, Int)
+hex = StaticModel ws v r
