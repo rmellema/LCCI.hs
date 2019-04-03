de8b59d04a3c078c00c2a2847b0870cc4f1a1f97
rmellema <rene.mellema@gmail.com>
2019-04-03 18:40:40 +0200

    Added the evaluation for the update operator

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -4,15 +4,19 @@ This module holds the code for the evaluation of LCCI formulas in models.
 module Evaluation (
     compoundRelation,
     compoundRelation',
-    supports,
+    supports, (|=),
+    productUpdate, (@@),
+    updatedState, (.@),
 ) where
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import Data.Maybe (fromMaybe)
+import Prelude hiding (lookup)
 
 import Issue
 import Model
 import Relation
+import Substitution hiding (lookup)
 import Syntax
 
 compoundRelation' :: (Ord a) => StaticModel a -> Program -> Relation a
@@ -44,6 +48,50 @@ compoundRelation m (Iterate p) s = Relation.lookup r s
 union' :: (Ord a) => Set.Set (State a) -> State a
 union' = Set.foldr Set.union Set.empty
 
+proj :: (Ord a, Ord b) => State (World a, b) -> (State a, Set.Set b)
+proj s = (Set.map (\(World (w, _)) -> w) s, Set.map (\(World (_, e)) -> e) s)
+
+proj1 :: (Ord a, Ord b) => State (World a, b) -> State a
+proj1 = fst . proj
+
+proj2 :: (Ord a, Ord b) => State (World a, b) -> Set.Set b
+proj2 = snd . proj
+
+pre :: UpdateModel -> Event -> Formula
+pre u e = precondition u Map.! e
+
+sub :: UpdateModel -> Event -> Substitution
+sub u e = substitutions u Map.! e
+
+relUpdate :: (Ord a) => State (World a, Event) -> Atomic -> StaticModel a -> UpdateModel -> Relation (World a, Event)
+relUpdate ws a m u = Relation.fromList
+        [(state [World (w, e)], t) | World (w, e) <- Set.toList ws, t <- Set.toList $ Set.powerSet ws,
+                               proj1 t `Set.member` lookup ra (state [w]),
+                               proj2 t `Set.member` (sa Map.! e)]
+        where ra = relation m Map.! a
+              sa = statemap u Map.! a
+
+productUpdate :: (Ord a) => StaticModel a -> UpdateModel -> StaticModel (World a, Event)
+productUpdate m u = StaticModel ws v r
+    where ws = Set.fromList [World (w, e) | w <- Set.toList $ worlds m,
+                                      e <- Set.toList $ events u,
+                                    (m, state [w]) |= pre u e]
+          v p (World (w, e)) = (m, state [w]) |= (sub u e ! p)
+          r = Map.fromList [(a, relUpdate ws a m u) | a <- Map.keys (relation m)]
+
+infix 1 @@
+(@@) :: (Ord a) => StaticModel a -> UpdateModel -> StaticModel (World a, Event)
+(@@) = productUpdate
+
+updatedState :: (Ord a) => StaticModel a -> State a -> UpdateModel -> [Event] -> State (World a, Event)
+updatedState m s u es = state [World (w, e) | w <- Set.toList $worlds m,
+                                       e <- es,
+                                        (m, state[w]) |= pre u e]
+
+infix 1 .@
+(.@) :: (Ord a) => (StaticModel a, State a) -> (UpdateModel, [Event]) -> State (World a, Event)
+(.@) (m, s) (u, es) = updatedState m s u es
+
 supports' :: (Ord a) => StaticModel a -> State a -> Formula -> Bool
 supports' m s (Prop p) = all (val p) s
     where val = valuation m
@@ -55,6 +103,7 @@ supports' m s (Cond f1 f2) = all cond $ powerset s
 supports' m s (Modal p f) = all (\w -> supports' m (t w) f) s
     where t w = union' $ compoundRelation m p $ state [w]
 supports' m s (IModal p f) = all (\t -> supports' m t f) $ compoundRelation m p s
+supports' m s (Update (_, u) es f) = (m @@ u, (m, s) .@ (u, es)) |= f
 
 supports :: (Ord a) => StaticModel a -> State a -> Formula -> Bool
 supports m s f = supports' m s (expand f)
